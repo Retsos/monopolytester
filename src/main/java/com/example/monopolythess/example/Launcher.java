a7966e53da361b3ba7da52b573dcbec13804ff67
Retsos <GeorgeRets@gmail.com>
2024-11-22 23:32:42 +0200

    balance on maps

--- src/main/java/com/example/monopolythess/example/Launcher.java
+++ src/main/java/com/example/monopolythess/example/Launcher.java
@@ -2,8 +2,4 @@ package com.example.monopolythess.example;
 
 import javafx.application.Application;
 
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class);
-    }
-}
+public class Launcher { public static void main(String[] args) {Application.launch(HelloApplication.class);}}
